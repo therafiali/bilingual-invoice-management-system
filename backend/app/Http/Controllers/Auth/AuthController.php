4434172669f19bcd86932896badba3cd0fee3789
Muhammad Rafi Ali <therafiali@gmail.com>
2025-12-09 12:59:53 +0500

    add reset password route

--- backend/app/Http/Controllers/Auth/AuthController.php
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -9,6 +9,8 @@ use App\Http\Resources\AuthResource;
 use App\Services\AuthService;
 use App\Traits\ApiResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -60,5 +62,47 @@ class AuthController extends Controller
         auth()->user()->currentAccessToken()->delete();
 
         return $this->successMessage('Successfully logged out', 200);
+    }
+
+
+    public function resetPassword(Request $request)
+    {
+
+        $request->validate([
+            'old_password' => 'string|required|min:6',
+            'new_password' => 'string|required|min:6'
+        ]);
+
+        $user = Auth::user();
+
+        // check if password is valid
+        if (!Hash::check($request->old_password, $user->password)) {
+            return $this->errorResponse("The provided password is incorrect.", 401);
+        }
+
+        // Step 4: Check if new password is different from old
+        if (Hash::check($request->new_password, $user->password)) {
+            return $this->errorResponse(
+                'New password must be different from your current password.',
+                409
+            );
+        }
+
+        $user->tokens()->delete();
+
+        $user->password = Hash::make($request->new_password);
+
+        $user->save();
+
+        $newToken = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Password reset successfully.',
+            'data' => [
+                'user' => new AuthResource($user),
+                'token' => $newToken,
+            ],
+        ], 200);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,6 +19,7 @@ Route::prefix('v1')->group(function () {
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('auth/logout', [AuthController::class, 'logout']);
+        Route::post('auth/reset', [AuthController::class, 'resetPassword']);
 
         Route::prefix('page')->group(function () {
             Route::post('add', [PageController::class, 'addPage']);
